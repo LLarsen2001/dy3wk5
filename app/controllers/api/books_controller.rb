09fd502c360c08028f92af425c92d4bc7ad2e7b8
Lance Larsen <lancelarsen2001@gmail.com>
2022-06-14 11:47:22 -0600

    changes

--- app/controllers/api/books_controller.rb
+++ app/controllers/api/books_controller.rb
@@ -1,2 +1,53 @@
 class Api::BooksController < ApplicationController
+    before_action :set_author, except: [:all_book, :find_book]
+    before_action :set_book, only: [:show, :update, :destroy]
+
+    def all_book
+        render json: Book.all
+    end
+
+    def find_book
+        render json: Book.find(params[:book_id_please])
+    end
+    def index 
+        render json: @author.books
+    end
+
+    def show 
+        render json: @book
+    end
+
+    def create
+        @book = @author.books.new(book_params)
+        if(@book.save)
+            render json: @book
+        else
+            render json: @book.errors.full_message, status: 422
+        end
+    end
+
+    def update
+     
+        if(@book.update(book_params))
+            render json: @book
+        else
+            render json: @book.errors.full_message, status: 422
+        end
+    end
+ 
+    def destroy
+        render json: @book.destroy
+    end
+    private
+
+    def set_author 
+        @author = Author.find(params[:author_id])
+    end
+    def set_book
+        @book = @author.books.find(params[:id])
+    end
+    def book_params
+        params.require(:book).permit(:title, :genre)
+    end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,13 @@ Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
+ 
   # root "articles#index"
+  namespace :api do
+    resources :authors do
+      resources :books
+    end
+    get '/books', to: 'books#all_books'
+    get '/books/:book_id_please', to: 'books#find_book'
+  end
 end
